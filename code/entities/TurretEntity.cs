47a8c2c3a2ecc634b57ef7ac2278df722e4b644f
Conna Wiles <kurozael@gmail.com>
2021-09-30 17:25:51 +0100

    Added hv_turret Hammer entity

---
+++ code/entities/TurretEntity.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+using System;
+
+namespace Facepunch.Hover
+{
+	[Library( "hv_turret" )]
+	[Hammer.EditorModel( "models/tempmodels/generator/generator_temp.vmdl", FixedBounds = true )]
+	[Hammer.EntityTool( "Turret", "Hover", "Defines a point where a team's turret spawns" )]
+	public partial class TurretEntity : AnimEntity
+	{
+		[Property] public Team Team { get; set; }
+
+		public override void Spawn()
+		{
+			SetModel( "models/tempmodels/turret/turret.vmdl" );
+			SetupPhysicsFromModel( PhysicsMotionType.Static );
+
+			Transmit = TransmitType.Always;
+
+			if ( Team == Team.Blue )
+				RenderColor = Color.Blue;
+			else
+				RenderColor = Color.Red;
+
+			base.Spawn();
+		}
+
+		public override void OnKilled()
+		{
+			// TODO: Can it be killed separately to the generator?
+		}
+	}
+}
