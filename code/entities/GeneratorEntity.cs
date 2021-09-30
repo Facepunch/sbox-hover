9909efba4e8059a5b0cd8c02e54a75370f4a2347
Conna Wiles <kurozael@gmail.com>
2021-09-30 15:06:06 +0100

    Added hv_generator Hammer entity to define team generator spawnpoints

---
+++ code/entities/GeneratorEntity.cs
@@ -0,0 +1,32 @@
+﻿using Sandbox;
+using System;
+
+namespace Facepunch.Hover
+{
+	[Library( "hv_generator" )]
+	[Hammer.EditorModel( "models/tempmodels/generator/generator_temp.vmdl", FixedBounds = true )]
+	[Hammer.EntityTool( "Generator", "Hover", "Defines a point where a team's generator spawns" )]
+	public partial class GeneratorEntity : ModelEntity
+	{
+		public delegate void GeneratorEvent( GeneratorEntity generator );
+		public static event GeneratorEvent OnGeneratorRepaired;
+		public static event GeneratorEvent OnGeneratorBroken;
+
+		[Property] public Team Team { get; set; }
+
+		public override void Spawn()
+		{
+			SetModel( "models/tempmodels/generator/generator_temp.vmdl" );
+			SetupPhysicsFromModel( PhysicsMotionType.Static );
+
+			Transmit = TransmitType.Always;
+
+			if ( Team == Team.Blue )
+				RenderColor = Color.Blue;
+			else
+				RenderColor = Color.Red;
+
+			base.Spawn();
+		}
+	}
+}
