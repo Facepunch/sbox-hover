a022047f201a03f448436a9177d5fa879aafc6a1
Conna Wiles <kurozael@gmail.com>
2021-10-05 14:01:47 +0100

    Bind particles to launch pad entity when using it

--- code/entities/LaunchPad.cs
+++ code/entities/LaunchPad.cs
@@ -43,8 +43,7 @@ namespace Facepunch.Hover
 
 				if ( IsServer )
 				{
-					var effect = Particles.Create( "particles/launch_pad/launch_pad_horizontal_jump.vpcf" );
-					effect.SetPosition( 0, Position );
+					var effect = Particles.Create( "particles/launch_pad/launch_pad_horizontal_jump.vpcf", this );
 					effect.SetPosition( 1, RenderColor * 255f );
 				}
 
