4ebd94becea495a67bd9936c14334bae96cbb35f
Conna Wiles <kurozael@gmail.com>
2021-10-12 22:40:23 +0100

    Added release.js node script to build .zip file of compiled files and source code only

---
+++ release.js
@@ -0,0 +1,32 @@
+var fs = require( "fs" );
+var archiver = require( "archiver" );
+var gitignore = fs.readFileSync( ".gitignore" ).toString().split( "\n" );
+
+for ( var i in gitignore ) {
+    var line = gitignore[i];
+    console.log( line );
+}
+
+var output = fs.createWriteStream( "release.zip" );
+var archive = archiver( "zip", {
+    zlib: { level: 9 }
+});
+
+output.on( "close" , function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log( "archiver has been finalized and the output file descriptor has closed." );
+});
+
+archive.on( "error" , function(err) {
+    throw err;
+});
+
+archive.pipe( output );
+
+archive.glob( "**/{*.cs,*.scss,*.vmdl_c,*.vmat_c,*.vsnd_c,*.sound_c,*.vfx,*.png,*.vpcf_c,*.vtex_c,*.addon,*.fgd,!.git}", {
+    ignore: [
+        "_bakeresourcecache/**/*.*"
+    ]
+});
+
+archive.finalize();
