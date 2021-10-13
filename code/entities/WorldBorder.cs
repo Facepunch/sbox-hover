9881bbd891bc1ded1d9d8f4049539b688e3e7ad4
Conna Wiles <kurozael@gmail.com>
2021-10-13 15:54:48 +0100

    Added WorldBorder Hammer entity

---
+++ code/entities/WorldBorder.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+
+namespace Facepunch.Hover
+{
+	[Library( "hv_world_border" )]
+	[Hammer.AutoApplyMaterial( "materials/map_border.vmat" )]
+	[Hammer.Solid]
+	public partial class WorldBorder : ModelEntity
+	{
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			SetupPhysicsFromModel( PhysicsMotionType.Static );
+			Transmit = TransmitType.Always;
+		}
+
+		public override void ClientSpawn()
+		{
+			var sceneObject = SceneObject;
+
+			sceneObject.Flags.IsOpaque = false;
+			sceneObject.Flags.IsTranslucent = true;
+			sceneObject.Flags.IsDecal = false;
+			sceneObject.Flags.OverlayLayer = false;
+			sceneObject.Flags.BloomLayer = false;
+			sceneObject.Flags.ViewModelLayer = false;
+			sceneObject.Flags.SkyBoxLayer = false;
+			sceneObject.Flags.NeedsLightProbe = false;
+
+			base.ClientSpawn();
+		}
+	}
+}
