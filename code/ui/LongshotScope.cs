abbb5806035c245048777923a5e287676ddd4d48
Conna Wiles <kurozael@gmail.com>
2021-10-15 19:28:53 +0100

    Slightly better scope but needs work

--- code/ui/LongshotScope.cs
+++ code/ui/LongshotScope.cs
@@ -9,6 +9,10 @@ namespace Facepunch.Hover
 	{
 		public static LongshotScope Instance { get; private set; }
 
+		public Panel Left { get; private set; }
+		public Panel Right { get; private set; }
+		public Panel Top { get; private set; }
+		public Panel Bottom { get; private set; }
 		public Image Inner { get; private set; }
 
 		public void Show()
@@ -25,11 +29,48 @@ namespace Facepunch.Hover
 		{
 			StyleSheet.Load( "/ui/LongshotScope.scss" );
 
+			Left = Add.Panel( "left" );
+			Right = Add.Panel( "right" );
+			Top = Add.Panel( "top" );
+			Bottom = Add.Panel( "bottom" );
 			Inner = Add.Image( "ui/scope_inner.png", "inner" );
 
 			Instance = this;
 
 			Hide();
+		}
+
+		public override void Tick()
+		{
+			var screenSize = Screen.Size * ScaleFromScreen;
+			var scopeSize = screenSize.y * 0.5f;
+
+			Inner.Style.Width = scopeSize;
+			Inner.Style.Height = scopeSize;
+			Inner.Style.Left = Length.Pixels( screenSize.x / 2f );
+			Inner.Style.Top = Length.Pixels( screenSize.y / 2f );
+
+			Left.Style.Width = ((screenSize.x * 0.5f) - (scopeSize * 0.5f)) + 1f;
+			Left.Style.Top = scopeSize * 0.5f;
+			Left.Style.Left = 0f;
+			Left.Style.Height = screenSize.y - (scopeSize * 0.5f);
+
+			Right.Style.Width = Left.Style.Width;
+			Right.Style.Left = ((screenSize.x * 0.5f) + (scopeSize * 0.5f)) - 1f;
+			Right.Style.Top = scopeSize * 0.5f;
+			Right.Style.Height = screenSize.y - (scopeSize * 0.5f);
+
+			Top.Style.Top = 0f;
+			Top.Style.Width = screenSize.x;
+			Top.Style.Height = (scopeSize * 0.5f) + 1f;
+			Top.Style.Left = 0f;
+
+			Bottom.Style.Bottom = 0f;
+			Bottom.Style.Width = screenSize.x;
+			Bottom.Style.Height = (scopeSize * 0.5f) + 1f;
+			Bottom.Style.Left = 0f;
+
+			base.Tick();
 		}
 	}
 }
