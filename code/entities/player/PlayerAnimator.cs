6d5a7d15c5f603da7b9c70bd750a4bd352f91d69
Conna Wiles <kurozael@gmail.com>
2021-10-23 13:10:57 +0100

    Skid lerp tweaks

--- code/entities/player/PlayerAnimator.cs
+++ code/entities/player/PlayerAnimator.cs
@@ -12,10 +12,17 @@ namespace Facepunch.Hover
 		{
 			base.Simulate();
 
-			if ( !HasTag( "skiing" ) && Input.Forward == 0f && Input.Left == 0f )
-				Skid = Skid.LerpTo( 1f, Time.Delta * 5f );
+			if ( Velocity.Length > 90f )
+			{
+				if ( !HasTag( "skiing" ) && Input.Forward == 0f && Input.Left == 0f )
+					Skid = Skid.LerpTo( 1f, Time.Delta * 5f );
+				else
+					Skid = Skid.LerpTo( 0f, Time.Delta * 5f );
+			}
 			else
+			{
 				Skid = Skid.LerpTo( 0f, Time.Delta * 5f );
+			}
 
 			SetParam( "skid", Skid );
 		}
