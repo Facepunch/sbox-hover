3dd4dae72f97062abf949886af62ceb923139421
kurozael <kurozael@gmail.com>
2023-09-04 20:34:15 +0100

    Safety checks for ToastList instance

--- code/ui/Hud.Network.cs
+++ code/ui/Hud.Network.cs
@@ -7,19 +7,19 @@ public partial class Hud
 	[ClientRpc]
 	public static void AddKillFeed( HoverPlayer attacker, HoverPlayer victim, Entity weapon )
 	{
-		ToastList.Instance.AddKillFeed( attacker, victim, weapon );
+		ToastList.Instance?.AddKillFeed( attacker, victim, weapon );
 	}
 
 	[ClientRpc]
 	public static void AddKillFeed( Entity attacker, HoverPlayer victim )
 	{
-		ToastList.Instance.AddKillFeed( attacker, victim );
+		ToastList.Instance?.AddKillFeed( attacker, victim );
 	}
 
 	[ClientRpc]
 	public static void AddKillFeed( HoverPlayer victim )
 	{
-		ToastList.Instance.AddKillFeed( victim );
+		ToastList.Instance?.AddKillFeed( victim );
 	}
 
 	public static void ToastAll( string text, string icon = "" )
@@ -35,6 +35,6 @@ public partial class Hud
 	[ClientRpc]
 	public static void Toast( string text, string icon = "" )
 	{
-		ToastList.Instance.AddItem( text, Texture.Load( FileSystem.Mounted, icon ) );
+		ToastList.Instance?.AddItem( text, Texture.Load( FileSystem.Mounted, icon ) );
 	}
 }
